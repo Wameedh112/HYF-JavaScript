a02c4427d874f4b33b53b7be68202c1048c5a4ae
Claudio Reggiani <wameedh.ali@hotmail.com>
2018-06-20 22:16:10 +0200

    homework-Js(for-loop)

---
+++ javascript.js
@@ -0,0 +1,7 @@
+for (let i = 1 ; i <= 100 ; i++ ){
+ 
+    if (i % 3===0){console.log("Hack");}
+    else if (i%5===0){console.log("FUTURE");}
+    else{console.log (i);}
+    if (i % 3 ===0 && i % 5 ===0){ console.log ("HACKYOURFUTURE");}
+}
